b6632dcbe4bba39d39398b8bdcc196881b8a4578
ladaoot <ladachumakova@yandex.ru>
2023-07-09 15:56:22 +0300

    homework 3 rustlings (strings, modules, hashmaps)

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,35 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        if let Option::None = scores.get(&team_1_name) {
+            scores.insert(team_1_name, Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+        } else {
+            let scored = scores.get(&team_1_name).unwrap().goals_scored + team_1_score;
+            let concered = scores.get(&team_1_name).unwrap().goals_conceded + team_2_score;
+            scores.insert(team_1_name, Team {
+                goals_scored: scored,
+                goals_conceded: concered
+            });
+        }
+
+        if let Option::None = scores.get(&team_2_name) {
+            scores.insert(team_2_name, Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+        } else {
+            let scored = scores.get(&team_2_name).unwrap().goals_scored + team_2_score;
+            let concered = scores.get(&team_2_name).unwrap().goals_conceded + team_1_score;
+            scores.insert(team_2_name, Team {
+                goals_scored: scored,
+                goals_conceded: concered
+            });
+        }
+
     }
     scores
 }
